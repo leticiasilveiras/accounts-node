4c44a21b4efc3cf6bf5d0c233acf8c75952e5251
Leticia Silveira <lesilveiras18@gmail.com>
2024-06-26 16:19:00 -0300

    Criação de conta

--- ACCOUNTS/index.js
+++ ACCOUNTS/index.js
@@ -19,7 +19,54 @@ function operation(){
     .then((answer) => {
         const action = answer['action'];
 
-        console.log(action);
+        if(action ===  'Criar Conta'){
+            createAccount();
+        }
     })
     .catch((err) => console.log(err));
+}
+
+//create account
+function createAccount() {
+    console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco. Seja bem-vindo!'));
+    console.log(chalk.green('Defina as opções da sua conta a seguir.'));
+
+    buildAccount();
+}
+
+function buildAccount() {
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Digite um nome para a sua conta: '
+        }
+    ])
+    .then(answer => {
+        const accountName = answer['accountName'];
+
+        console.log(accountName);
+
+        if(!fs.existsSync('accounts')){
+            fs.mkdirSync('accounts');
+        }
+
+        if(fs.existsSync(`accounts/${accountName}.json`)){
+            console.log(
+                chalk.bgRed.black('Esta conta já existe, escolha outro nome!')
+            )
+            buildAccount();
+            return;
+        }
+
+        fs.writeFileSync(
+            `accounts/${accountName}.json`, 
+            '{"balance": 0}', 
+            function(err){
+                console.log(err);
+        })
+
+        console.log(chalk.green('Parabéns, a sua conta foi criada!'));
+        operation();
+    })
+    .catch(err => console.log(err));
 }
